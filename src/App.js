d05611cd5f5b476d58ab44bdc4fe6ef954d65b3c
20minhajj <minhajjharith@gmail.com>
2020-12-15 17:45:06 +0300

    Page navigation"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <nav className="navbar navbar-light bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand text-light text-pad" href="/">Restaurant Review</a>
+          <form class="d-flex btn-pad">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search Restaurant"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success " type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
     </div>
   );
 }
