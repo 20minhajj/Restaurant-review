3979a5a98fe7551b917b2b788bd2b960b0cf569c
20minhajj <minhajjharith@gmail.com>
2021-01-25 22:15:28 +0300

    setting add rendering of the app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import Map from './Components/Map'
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-
+import Maps from './components/maps'
 function App() {
   return (
     <div className="container-fluid">
       <nav className="navbar navbar-light bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand text-light text-pad" href="/">Restaurant Review</a>
-          <form class="d-flex btn-pad">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search Restaurant"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success " type="submit">
-              Search
-            </button>
-          </form>
+          <a className="navbar-brand text-light text-pad" href="/">
+            Restaurant Review
+          </a>
         </div>
       </nav>
-       <Map />
+      <Maps />
     </div>
   );
 }
